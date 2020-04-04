c41fa5ae9ef72408c4ae124def469b8ae6418fea
rohan-kshah <40341691+rohan-kshah@users.noreply.github.com>
2020-04-04 00:10:15 -0700

    Add files via upload

--- bfumarole.c
+++ bfumarole.c
@@ -1,11 +1,19 @@
+// clang bfumarole.c -o bfumarole
+// ./bfumarole file.bf.txt
+// above is how to run. uses compiler to generate you name-specified output, then runs it.
+// Author: Trent Conley and Rohan Shah
+// Date: April 3, 2020
+// Class: ATCS, Block G and Block B
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdbool.h"
 #include "string.h"
 
-#define TSIZE 50000
 #define MAXSIZE 10000
+#define NUM_BYTES 50
 
+//declaring all valid characters as constants
 const char lc = '<';
 const char rc = '>';
 const char a = '+';
@@ -14,21 +22,199 @@ const char lp = '(';
 const char rp = ')';
 const char ast = '*';
 
+void plus(char arr[], int* pos_a)
+{
+  arr[*pos_a] = arr[*pos_a] + 1;
+}
+
+void minus(char arr[], int* pos_a)
+{
+  arr[*pos_a] = arr[*pos_a] - 1;
+}
+
+
+void mov_L(char arr[], int* pos_a)
+{
+  if (*pos_a == 0)
+  {
+    printf("You can't do that. You can't move to the left of the left most person. \n \n");
+      exit(0);
+  }
+    //no else here because i want the program to break.
+
+  *pos_a = *pos_a - 1;
+    //above is to kill the program if bad.
+}
+
+void mov_R(char arr[], int* pos_a)
+{
+//    printf("%d\n", *pos_a);
+    if (*pos_a == NUM_BYTES-1)
+    {
+        printf("You can't do that. You can't move to the right of the right most person. \n \n");
+        exit(0);
+    }
+    //no else here because i want the program to break.
+    *pos_a = *pos_a + 1;
+    //above is to kill the program if bad.
+}
+
+void findclose(char tablet[], int* pos_t)
+{
+    int c = 1;
+    while (c > 0)
+    {
+        *pos_t = *pos_t + 1;
+        if (tablet[*pos_t] == ')')
+        {
+            c = c - 1;
+        }
+        else if (tablet[*pos_t] == '(')
+        {
+            c = c + 1;
+        }
+    }
+}
+
+void findopen(char tablet[], int* pos_t)
+{
+
+    int c = 1;
+    while (c > 0)
+    {
+
+        *pos_t = *pos_t - 1;
+        if (tablet[*pos_t] == '(')
+        {
+            c = c - 1;
+        }
+        else if (tablet[*pos_t] == ')')
+        {
+            c = c + 1;
+        }
+    }
+}
+
+void open_p(char arr[], char tablet[], int* pos_a, int* pos_t)
+{
+    if (arr[*pos_a] == 0)
+    {
+        findclose(tablet, pos_t);
+        //auto- edits in the heap :)
+    }
+    else
+    {
+        *pos_t = *pos_t + 1;
+    }
+}
+
+void close_p(char arr[], char tablet[], int* pos_a, int* pos_t)
+{
+    if (arr[*pos_a] == 0)
+    {
+        *pos_t = *pos_t + 1;
+        //auto- edits in the heap :)
+    }
+    else
+    {
+        findopen(tablet, pos_t);
+
+    }
+}
+int negative(int num)
+{
+    return 256 + num;
+}
+
+
+void yell(char arr[], int* pos_a)
+{
+    char c;
+    if (arr[*pos_a] < (char) 0)
+    {
+        c = negative(arr[*pos_a]);
+    }
+    else
+    {
+        c = arr[*pos_a];
+    }
+    printf("%c", c);
+}
+
+void flow(char tablet[], char arrs[], int* pos_t, int* pos_a)
+{
+//    printf("halp");
+    char mychar = tablet[*pos_t];
+    if (mychar == '+')
+    {
+        plus(arrs, pos_a);
+        *pos_t = *pos_t + 1;
+
+    }
+    else if (mychar == '-')
+    {
+        minus(arrs, pos_a);
+        *pos_t = *pos_t + 1;
+    }
+    else if (mychar == '(')
+    {
+        open_p(arrs, tablet, pos_a, pos_t);
+    }
+    else if (mychar == ')')
+    {
+        close_p(arrs, tablet, pos_a, pos_t);
+    }
+    else if (mychar == '<')
+    {
+        mov_L(arrs, pos_a);
+        *pos_t = *pos_t + 1;
+    }
+    else if (mychar == '>')
+    {
+        mov_R(arrs, pos_a);
+        *pos_t = *pos_t + 1;
+    }
+    else if (mychar == '*')
+    {
+        yell(arrs, pos_a);
+        *pos_t = *pos_t + 1;
+    }
+}
+
+
+void init0(char a[], int size)
+{
+    for (int i = 0; i < size; i ++)
+    {
+        a[i] = 0;
+    }
+}
+
+
+void print(char a[], int size)
+{
+    for (int i = 0; i < size; i ++)
+    {
+        printf("%d\n", a[i]);
+    }
+}
+
 //This method checks if the given array of chars contains matching parentheses. 
-bool balanced_parentheses(char* ritual) {
+bool balanced_parentheses(char* r) {
   int counter = 0;
-  for(int i = 0; i < strlen(ritual); i = i + 1)
+
+  for(int i = 0; i < strlen(r); i = i + 1)
   {
     //if negative, a closed parentheses must be in front of a open parentheses
     if(counter < 0)
     {
       return false;
     } else {
-      if(ritual[i] == '(')
+      if(r[i] == '(')
       {
         counter = counter + 1;
       }
-      if(ritual[i] == ')')
+      if(r[i] == ')')
       {
         counter = counter - 1;
       }
@@ -43,10 +229,9 @@ bool balanced_parentheses(char* ritual) {
 
 int main(int argc, char** argv) 
 {
-  char file_contents[MAXSIZE]; //creating the array with all the valid chars in it
-  unsigned char tribe[TSIZE] = {0}; //unassigned array for the tribe ... wrapping taken care of
-
-  //opens and reads the filee
+  int ch = 0;
+  char tablet[MAXSIZE]; //creating the array with all the valid chars in it
+  //opens and reads the file
   FILE* fp = fopen("file.bf.txt", "r");
   if (fp == NULL)
   {
@@ -54,6 +239,7 @@ int main(int argc, char** argv)
     return(1);
   }
   int i = 0;
+  //running through entire file character by character
   while(feof(fp) == false) 
   {
     //taking one char at a time from the file
@@ -61,17 +247,48 @@ int main(int argc, char** argv)
     //checking if c is a valid character
     if((c == lc) || (c == rc) || (c == a) || (c == s) || (c == lp) || (c == rp) || (c == ast)) 
     {
-      file_contents[i] = c;
+      tablet[i] = c;
       i++;
+      ch++;
     }
   }
   fclose(fp);
 
-  if (balanced_parentheses(file_contents) == false) 
+  //checking to see if the array has no contents
+  if(ch == 0)
+  {
+    puts("No valid contents inside the file.");
+    return(0);
+  }
+
+  if (balanced_parentheses(tablet) == false) 
   {
     puts("Invalid file. Parentheses do not match.");
     return(0);
   }
-//execute code here
+
+  //declaring some shit
+  int pos_tablet = 0;
+  int *pos_tablet_ptr = malloc(sizeof(int));
+  *pos_tablet_ptr = 0;
+
+  int pos_arrs = 0;
+
+  int *pos_arr_ptr = malloc(sizeof(int));
+  *pos_arr_ptr = 0;
+    //printf(*pos_arr_ptr);
+
+  char arrs[NUM_BYTES];
+  init0(arrs, sizeof(arrs));
+  int size = sizeof(tablet)-1;
+  while ( (int) *pos_tablet_ptr < size)
+  {
+    flow(tablet, arrs, pos_tablet_ptr, pos_arr_ptr);
+  }
+
+//print(arrs, sizeof(arrs));
+  free(pos_arr_ptr);
+  free(pos_tablet_ptr);
+
   return(0);
 }
